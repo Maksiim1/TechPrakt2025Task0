9b29839de32247f46026db1cd3cb8a07561d0c04
Maxim Rahimov <ipz235_rmya@student.ztu.edu.ua>
2025-02-06 03:01:46 +0200

    second

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        Console.Write("Введіть перше число: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введіть друге число: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double sum = num1 + num2;
+
+        Console.WriteLine($"Сума чисел {num1} і {num2} дорівнює: {sum}");
+    }
+}
