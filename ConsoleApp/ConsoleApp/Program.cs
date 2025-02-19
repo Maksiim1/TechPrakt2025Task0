03457fca563816747a19d1313687048d5a08a126
Maksiim1 <148970452+Maksiim1@users.noreply.github.com>
2025-02-19 07:44:09 +0200

    fixing the lack of validation of entered numbers

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Text;
+using System.Globalization;
 
 class Program
 {
@@ -7,14 +8,22 @@ class Program
     {
         Console.OutputEncoding = Encoding.UTF8;
 
-        Console.Write("Введіть перше число: ");
-        double num1 = Convert.ToDouble(Console.ReadLine());
-
-        Console.Write("Введіть друге число: ");
-        double num2 = Convert.ToDouble(Console.ReadLine());
+        double num1 = ReadNumber("Введіть перше число: ");
+        double num2 = ReadNumber("Введіть друге число: ");
 
         double sum = num1 + num2;
-
         Console.WriteLine($"Сума чисел {num1} і {num2} дорівнює: {sum}");
+    }
+
+    static double ReadNumber(string message)
+    {
+        double number;
+        Console.Write(message);
+        while (!double.TryParse(Console.ReadLine(), NumberStyles.Any, CultureInfo.InvariantCulture, out number))
+        {
+            Console.WriteLine("Помилка! Введіть коректне число.");
+            Console.Write(message);
+        }
+        return number;
     }
 }
